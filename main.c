1a2121dacd97d60da2e8725341c5209ee16317b0
Nilay Sardar <83805097+Nilaysardar1998@users.noreply.github.com>
2021-09-05 19:46:38 +0530

    some correction in Surface Area code for cone

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-
+#include <math.h>
 int main() {
-    printf("Hello, World!\n");
+    float radius, height; double pie = 3.14;
+
+    printf("\nEnter the Radius of Cone: ");
+    scanf_s("%f", &radius);
+
+    printf("\nEnter the Height of Cone: ");
+    scanf_s("%f", &height);
+
+    double surface_area =  pie * radius *(radius + sqrtf(radius * radius + height * height));
+    printf("\nArea of Cone: %.2lf", surface_area);
     return 0;
 }
